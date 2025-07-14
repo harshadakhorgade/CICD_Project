48241c12b8c75d688d504c281e8d53224ae5b944
harshadakhorgade <harshadakhorgade9@gmail.com>
2025-07-14 12:49:03 +0530

    update

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.book_list, name='book_list'),
-    path('book/<int:pk>/', views.book_detail, name='book_detail'),
-    path('order/create/<int:book_id>/', views.create_order, name='create_order'),
-    path('order/<int:order_id>/', views.order_detail, name='order_detail'),
-    path('orders/', views.order_list, name='order_list'),
-    path('stats/', views.stats_view, name='stats'),
+#     path('book/<int:pk>/', views.book_detail, name='book_detail'),
+#     path('order/create/<int:book_id>/', views.create_order, name='create_order'),
+#     path('order/<int:order_id>/', views.order_detail, name='order_detail'),
+#     path('orders/', views.order_list, name='order_list'),
+#     path('stats/', views.stats_view, name='stats'),
 ]
